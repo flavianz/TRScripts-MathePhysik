3a4ffa50af5d3b718ce1d93708686bcbf8e7bea0
Flavianthepavian <survivallist.net@gmail.com>
2023-11-14 22:26:06 +0100

    Remove fstring

--- schiefer_wurf.py
+++ schiefer_wurf.py
@@ -23,7 +23,7 @@ if mode == "1" or mode == "1 ":
     winkelt = math.degrees(math.atan(vy / v0x))
     x = v0 * math.cos(math.radians(winkel)) * t
     y = v0 * math.sin(math.radians(winkel)) * t - (1 / 2) * 9.81 * (t ** 2)
-    print(f"\nv: {str(v)}\na: {str(winkelt)}\nx: {str(x)}\ny: {str(y)}\nVy: {str(vy)}\nVx: {str(v0x)}\nV0y: {str(v0y)}\nV0x: {str(v0x)}\nt: {str(t)}\n")
+    print("\nv: " + str(v) + "\na: " + str(winkelt) + "\nx: " + str(x) + "\ny: " + str(y) + "\nVy: " + str(vy) + "\nVx: " + str(v0x) + "\nV0y: " + str(v0y) + "\nV0x: " + str(v0x) + "\nt: " + str(t) + "\n")
 
 else:
     v0 = input("v0 in m/s: ")
@@ -38,15 +38,15 @@ else:
             if not is_empty(xw):
                 sin = math.sin(2 * math.radians(float(winkel)))
                 v0 = math.sqrt(float(xw) * 9.81 / sin)
-                print(f"\nv0 = sqrt({xw} * 9.81 / sin(2 * {winkel}))")
+                print("\nv0 = sqrt(" + xw + " * 9.81 / sin(2 * " + winkel + "))")
             elif not is_empty(ymax):
                 sin = math.sin(math.radians(float(winkel)))
                 sqrt = math.sqrt(float(ymax) * 19.62)
                 v0 = sqrt / sin
-                print(f"\nv0 = sqrt({ymax} * 19.62 / sin({winkel}))")
+                print("\nv0 = sqrt(" + ymax + " * 19.62 / sin(" + winkel + "))")
             elif not is_empty(t):
                 v0 = (float(t) * 9.81) / (2 * math.sin(math.radians(float(winkel))))
-                print(f"\nv0 = ({t} * 9.81) / (2 * sin({winkel}))")
+                print("\nv0 = (" + t + " * 9.81) / (2 * sin(" + winkel + "))")
     if is_empty(winkel):
         if is_empty(v0):
             winkel = ""
@@ -54,28 +54,28 @@ else:
             if not is_empty(xw):
                 inner = float(xw) * 9.81 / (float(v0) ** 2)
                 winkel = math.degrees(math.asin(inner)) / 2
-                print(f"\na = (sin-1(({xw} * 9.81) / ({v0} ^ 2))")
+                print("\na = (sin-1((" + xw + " * 9.81) / (" + v0 + " ^ 2))")
             elif not is_empty(ymax):
                 sqrt = math.sqrt(float(ymax) * 19.62)
                 winkel = math.degrees(math.asin(sqrt / float(v0)))
-                print(f"\na = sin-1((sqrt({ymax} * 19.62) / {v0})")
+                print("\na = sin-1((sqrt(" + ymax + " * 19.62) / " + v0 + ")")
             elif not is_empty(t):
                 inner = (float(t) * 9.81) / (2 * float(v0))
                 winkel = math.degrees(math.asin(inner))
-                print(f"\na = sin-1(({t} * 9.81) / (2 * {v0}))")
+                print("\na = sin-1((" + t + " * 9.81) / (2 * " + v0 + "))")
     if is_empty(t):
         if not is_empty(v0) and not is_empty(winkel):
             t = 2 * float(v0) * math.sin(math.radians(float(winkel))) / 9.81
-            print(f"\nt = 2 * {v0} * sin({winkel}) / 9.81")
+            print("\nt = 2 * " + v0 + " * sin(" + winkel + ") / 9.81")
     if is_empty(ymax):
         if not is_empty(v0) and not is_empty(winkel):
 
             ymax = (((float(v0) * math.sin(math.radians(float(winkel)))) ** 2) / 19.62)
-            print(f"\nY-max = {v0} * (sin({winkel}) ^ 2) / 19.62")
+            print("\nY-max = " + v0 + " * (sin(" + winkel + ") ^ 2) / 19.62")
     if is_empty(xw):
         if not is_empty(v0) and not is_empty(winkel):
             xw = (float(v0) ** 2) * math.sin(math.radians(2 * float(winkel))) / 9.81
-            print(f"\nx-w = ({v0} ^ 2) * sin(2 * {winkel}) / 9.81")
+            print("\nx-w = (" + v0 + " ^ 2) * sin(2 * " + winkel + ") / 9.81")
 
 
-    print(f"\nv0: {str(v0)}\na: {str(winkel)}\nt: {str(t)}\nY-max: {str(ymax)}\nX-w: {str(xw)}")
+    print("\nv0: " + str(v0) + "\na: " + str(winkel) + "\nt: " + str(t) + "\nY-max: " + str(ymax) + "\nX-w: " + str(xw) + "")
