e917ff2610e675f8934de3e0af910ce160c58a2e
Flavian Züllig <flavianzuellig@hotmail.com>
2023-11-14 22:35:06 +0100

    Update schiefer_wurf.py

--- schiefer_wurf.py
+++ schiefer_wurf.py
@@ -39,15 +39,15 @@ while True:
                 if not is_empty(xw):
                     sin = math.sin(2 * math.radians(float(winkel)))
                     v0 = math.sqrt(float(xw) * 9.81 / sin)
-                    print("\nv0 = sqrt(" + xw + " * 9.81 / sin(2 * " + winkel + "))")
+                    print("\nv0 = sqrt(" + str(xw) + " * 9.81 / sin(2 * " + str(winkel) + "))")
                 elif not is_empty(ymax):
                     sin = math.sin(math.radians(float(winkel)))
                     sqrt = math.sqrt(float(ymax) * 19.62)
                     v0 = sqrt / sin
-                    print("\nv0 = sqrt(" + ymax + " * 19.62 / sin(" + winkel + "))")
+                    print("\nv0 = sqrt(" + str(ymax) + " * 19.62 / sin(" + str(winkel) + "))")
                 elif not is_empty(t):
                     v0 = (float(t) * 9.81) / (2 * math.sin(math.radians(float(winkel))))
-                    print("\nv0 = (" + t + " * 9.81) / (2 * sin(" + winkel + "))")
+                    print("\nv0 = (" + str(t) + " * 9.81) / (2 * sin(" + str(winkel) + "))")
         if is_empty(winkel):
             if is_empty(v0):
                 winkel = ""
@@ -55,24 +55,24 @@ while True:
                 if not is_empty(xw):
                     inner = float(xw) * 9.81 / (float(v0) ** 2)
                     winkel = math.degrees(math.asin(inner)) / 2
-                    print("\na = (sin-1((" + xw + " * 9.81) / (" + v0 + " ^ 2))")
+                    print("\na = (sin-1((" + str(xw) + " * 9.81) / (" + str(v0) + " ^ 2))")
                 elif not is_empty(ymax):
                     sqrt = math.sqrt(float(ymax) * 19.62)
                     winkel = math.degrees(math.asin(sqrt / float(v0)))
-                    print("\na = sin-1((sqrt(" + ymax + " * 19.62) / " + v0 + ")")
+                    print("\na = sin-1((sqrt(" + str(ymax) + " * 19.62) / " + str(v0) + ")")
                 elif not is_empty(t):
                     inner = (float(t) * 9.81) / (2 * float(v0))
                     winkel = math.degrees(math.asin(inner))
-                    print("\na = sin-1((" + t + " * 9.81) / (2 * " + v0 + "))")
+                    print("\na = sin-1((" + str(t) + " * 9.81) / (2 * " + str(v0) + "))")
         if is_empty(t):
             if not is_empty(v0) and not is_empty(winkel):
                 t = 2 * float(v0) * math.sin(math.radians(float(winkel))) / 9.81
-                print("\nt = 2 * " + v0 + " * sin(" + winkel + ") / 9.81")
+                print("\nt = 2 * " + str(v0) + " * sin(" + str(winkel) + ") / 9.81")
         if is_empty(ymax):
             if not is_empty(v0) and not is_empty(winkel):
 
                 ymax = (((float(v0) * math.sin(math.radians(float(winkel)))) ** 2) / 19.62)
-                print("\nY-max = " + v0 + " * (sin(" + winkel + ") ^ 2) / 19.62")
+                print("\nY-max = " + str(v0) + " * (sin(" + str(winkel) + ") ^ 2) / 19.62")
         if is_empty(xw):
             if not is_empty(v0) and not is_empty(winkel):
                 xw = (float(v0) ** 2) * math.sin(math.radians(2 * float(winkel))) / 9.81
